a36c524100a21821c21b4fcfe2f8b4f7fca30823
Caleb Makoma <makomapince@gmail.com>
2025-07-27 00:21:42 +0300

    Initial commit with Attributes.java

---
+++ Attributes.java
@@ -0,0 +1,8 @@
+public class Attributes {
+    int x = 10;
+
+    public static void main(String[] args) {
+        Attributes myObj = new Attributes();
+        System.out.println(myObj.x);
+    }
+}
